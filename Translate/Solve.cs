cab21e3ee8d3f4bf897a823ccbc2c0aba72ded6a
chrismo80 <krizm0@freenet.de>
2024-05-24 21:28:29 +0200

    update Solve.cs

--- Translate/Solve.cs
+++ Translate/Solve.cs
@@ -1,23 +1,27 @@
 ﻿namespace Language;
 
-using GTranslate.Translators;
-
 public static class Language
 {
     public static void Translate()
     {
         var translator = new GTranslate.Translators.BingTranslator();
 
-        var texts = new string[]
-        {
-             "hello world",
-             "timeout pos2",
-             "not enabled",
-             "com error",
-        };
-
+        var texts = System.IO.File.ReadAllLines("Translate/Cp.csv")
+            .Where(line => line.StartsWith("Stat.Errors"))
+            .Select(line => line.Split(';').First().TranslateDefault());
 
         foreach (var text in texts)
             Console.WriteLine(text + " = " + translator.TranslateAsync(text, "de").GetAwaiter().GetResult().Translation);
+    }
+
+    private static string TranslateDefault(this string name) =>
+        string.Join(" - ", name.Split('.').Select(word => word.MakeReadable()).Skip(2));
+
+    private static string MakeReadable(this string word) // "NotEnabled" -> "not enabled"
+    {
+        foreach (char letter in word.Where(c => char.IsUpper(c)))
+            word = word.Replace(letter.ToString(), $" {letter.ToString().ToLowerInvariant()}");
+
+        return word.Trim();
     }
 }
