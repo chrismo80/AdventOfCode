eebf0c5ccc5762bdcb9a251c198ba54585df5f60
chrismo80 <krizm0@freenet.de>
2024-05-25 08:10:22 +0200

    update Solve.cs

--- Translate/Solve.cs
+++ Translate/Solve.cs
@@ -6,16 +6,22 @@ public static class Language
     {
         var translator = new GTranslate.Translators.BingTranslator();
 
+        var langs = GTranslate.Language.LanguageDictionary;
+
+        foreach (var lang in langs)
+            Console.WriteLine(lang.Key + " - " + lang.Value.Name);
+
         var texts = System.IO.File.ReadAllLines("Translate/Cp.csv")
             .Where(line => line.StartsWith("Stat.Errors"))
-            .Select(line => line.Split(';').First().TranslateDefault());
+            .Select(line => line.Split(';').First().TranslateDefault())
+            .Distinct();
 
         foreach (var text in texts)
-            Console.WriteLine(text + " = " + translator.TranslateAsync(text, "de").GetAwaiter().GetResult().Translation);
+            Console.WriteLine(text + " = " + translator.TranslateAsync(text, "da").GetAwaiter().GetResult().Translation);
     }
 
     private static string TranslateDefault(this string name) =>
-        string.Join(" - ", name.Split('.').Select(word => word.MakeReadable()).Skip(2));
+        string.Join(" - ", name.Split('.').Select(word => word.MakeReadable()).Last());
 
     private static string MakeReadable(this string word) // "NotEnabled" -> "not enabled"
     {
