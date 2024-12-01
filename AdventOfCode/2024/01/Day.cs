dc7f5056f2e427d1d8c5cf2e4715de4af1f2f515
chrismo80 <krizm0@freenet.de>
2024-12-01 21:57:18 +0100

    Add day 1

---
+++ AdventOfCode/2024/01/Day.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode2024;
+using Extensions;
+public static class Day1
+{
+    public static void Solve()
+    {
+        Console.WriteLine(Directory.GetCurrentDirectory());
+
+        var input = File.ReadAllLines("AdventOfCode/2024/01/Input.txt")
+            .Select(x => x.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse))
+            .ToList();
+
+        var left = input.Select(x => x.First()).Order().ToList();
+        var right = input.Select(x => x.Last()).Order().ToList();
+
+        var distances = left.Zip(right, (l, r) => Math.Abs(l - r));
+        var similarities = left.Select(l => l * right.Count(r => r == l));
+        
+        Console.WriteLine($"Part 1: {distances.Sum()}, Part 2: {similarities.Sum()}");
+    }
+}
