83472d413f6e9313f7c1ffce7fb5e79b37bf5e99
chrismo80 <krizm0@freenet.de>
2024-12-02 22:13:12 +0100

    Prepare day 3

---
+++ AdventOfCode/2024/03/Day.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode2024;
+
+public static class Day3
+{
+    public static void Solve()
+    {
+        var input = File.ReadAllLines("AdventOfCode/2024/03/Input.txt");
+        
+        Console.WriteLine($"Part 1: {input.Count()}, Part 2: {input.Count()}");
+    }
+}
