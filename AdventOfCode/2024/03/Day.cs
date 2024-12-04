17a9a6e18e5f5623eece7a7c229ced52e034bcda
chrismo80 <krizm0@freenet.de>
2024-12-04 18:03:26 +0100

    Add day 3

--- AdventOfCode/2024/03/Day.cs
+++ AdventOfCode/2024/03/Day.cs
@@ -1,11 +1,44 @@
+using System.Text.RegularExpressions;
+using Extensions;
+
 namespace AdventOfCode2024;
 
 public static class Day3
 {
     public static void Solve()
     {
-        var input = File.ReadAllLines("AdventOfCode/2024/03/Input.txt");
+        var input = File.ReadAllText("AdventOfCode/2024/03/Input.txt");
+
+        var matches = new Regex(@"mul\(([0-9]+)\,([0-9]+)\)|do\(\)|don\'t\(\)").Matches(input);
         
-        Console.WriteLine($"Part 1: {input.Count()}, Part 2: {input.Count()}");
+        var result1 = Calculate(matches.Where(match => match.Value.StartsWith("mul")));
+        
+        bool enabled = true;
+        var instructions = new List<Match>();
+
+        foreach (Match match in matches)
+        {
+            switch (match.Value)
+            {
+                case "do()":
+                    enabled = true;
+                    break;
+                case "don't()":
+                    enabled = false;
+                    break;
+                default:
+                    if(enabled) 
+                        instructions.Add(match);
+                    break;
+            }
+        }
+
+        var result2 = Calculate(instructions);
+
+        Console.WriteLine($"Part 1: {result1}, Part 2: {Calculate(instructions)}");
     }
+
+    private static int Calculate(IEnumerable<Match> matches) =>
+        matches.Select(match => match.Groups.Values.Skip(1).Select(g => int.Parse(g.Value)).Product())
+            .Sum();
 }
