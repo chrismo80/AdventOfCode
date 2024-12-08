041773837634158ce836d92f48dfcd67229cf254
chrismo80 <krizm0@freenet.de>
2024-12-08 12:39:34 +0100

    Day 6

--- AdventOfCode/2024/06/Day.cs
+++ AdventOfCode/2024/06/Day.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Extensions;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 
 namespace AdventOfCode2024;
 
@@ -10,23 +11,54 @@ public static class Day6
 		var map = File.ReadAllLines("AdventOfCode/2024/06/Input.txt")
 			.Select(row => row.ToArray()).ToArray();
 
-		var grid = new PathFinding.Grid<char>()
+		var current = new PathFinding.Grid<char>() { Map = map }
+			.Find((value) => value != '.' && value != '#');
+
+		var direction = map[current.Y][current.X];
+
+		var visited = new HashSet<(int, int)>() { current };
+
+		while (true)
 		{
-			Map = map,
-			Walkable = (nX, nY, next, cX, cY, current, lX, lY, last) => next != '#' &&
-			(
-				(lX == cX && cX == nX) ||
-				(lY == cY && cY == nY)
-			)
-		};
+			var next = current.Next(direction);
 
-		var start = grid.Find((value) => value != '.' && value != '#');
-		var last = (start.X, start.Y + 1);
+			if (next.OutOfBounds(map))
+				break;
 
-		var path = grid.BreadthFirstSearch(start, (0, 0), last).ToList();
+			if (next.WallDetected(map))
+			{
+				direction = direction.TurnRight();
+				continue;
+			}
 
-		Console.WriteLine(grid.Print());
+			current = next;
+			visited.Add(current);
+		}
 
-		Console.WriteLine($"Part 1: {path.Count}, Part 2: {0}");
+		Console.WriteLine($"Part 1: {visited.Count}, Part 2: {0}");
 	}
+
+	private static bool OutOfBounds(this (int X, int Y) location, char[][] map) =>
+		location.X < 0 || location.Y < 0 || location.X >= map.Length || location.Y >= map[0].Length;
+
+	private static (int X, int Y) Next(this (int X, int Y) location, char direction) => direction switch
+	{
+		'^' => (X: location.X, Y: location.Y - 1),
+		'v' => (X: location.X, Y: location.Y + 1),
+		'<' => (X: location.X - 1, Y: location.Y),
+		'>' => (X: location.X + 1, Y: location.Y),
+		_ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+	};
+
+	private static bool WallDetected(this (int X, int Y) location, char[][] map) =>
+		map[location.Y][location.X] == '#';
+
+	private static char TurnRight(this char direction) => direction switch
+	{
+		'^' => '>',
+		'>' => 'v',
+		'v' => '<',
+		'<' => '^',
+		_ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+	};
 }
