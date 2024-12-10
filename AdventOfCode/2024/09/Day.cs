09f8fc5ad39c8e18589c4a56b25ae70ae7d44bd2
chrismo80 <krizm0@freenet.de>
2024-12-10 20:41:49 +0100

    Part 2

--- AdventOfCode/2024/09/Day.cs
+++ AdventOfCode/2024/09/Day.cs
@@ -8,13 +8,13 @@ public static class Day9
 			.GetDiskMap();
 
 		var result1 = diskmap.ToArray().MoveSingle().GetCheckSum();
-		var result2 = diskmap.ToArray().MoveBlock().GetCheckSum();
+		var result2 = diskmap.ToArray().MoveBlocks().GetCheckSum();
 
 		Console.WriteLine($"Part 1: {result1}, Part 2: {result2}");
 	}
 
 	private static long GetCheckSum(this char[] diskmap) =>
-		diskmap.Where(x => x != '.').Select((c, i) => (long)(i * (c - '0'))).Sum();
+		diskmap.Select((c, i) => c == '.' ? 0 : (long)(i * (c - '0'))).Sum();
 
 	private static string GetDiskMap(this string input) =>
 		string.Join("", input.Select((c, i) => c.Transform(i)));
@@ -23,25 +23,6 @@ public static class Day9
 		new(Enumerable.Repeat(index % 2 == 0 ? (char)('0' + index / 2) : '.', item - '0').ToArray());
 
 	private static char[] MoveSingle(this char[] list)
-	{
-		int left = 0, right = list.Length - 1;
-
-		while (left++ < right)
-		{
-			if (list[left] != '.')
-				continue;
-
-			while (list[right] == '.')
-				right--;
-
-			list[left] = list[right];
-			list[right] = '.';
-		}
-
-		return list;
-	}
-
-	private static char[] MoveBlock(this char[] list)
 	{
 		int left = 0, right = list.Length;
 
@@ -53,10 +34,85 @@ public static class Day9
 			while (list[left] != '.')
 				left++;
 
-			list[left] = list[right];
-			list[right] = '.';
+			list.Move(left, right);
 		}
 
 		return list;
+	}
+
+	private static void Move(this char[] list, int left, int right)
+	{
+		if (left > right)
+			return;
+
+		list[left] = list[right];
+		list[right] = '.';
+	}
+
+	private static char[] MoveBlocks(this char[] list)
+	{
+		var right = list.Length - 1;
+
+		while (right > 0)
+		{
+			if (list[right] == '.')
+			{
+				right--;
+				continue;
+			}
+
+			var length = list.FindBlock(right);
+
+			if (length == -1)
+				break;
+
+			var left = list.FindSpace(0, length);
+
+			if (left == -1)
+			{
+				right -= length;
+				continue;
+			}
+
+			while (length-- > 0)
+				list.Move(left++, right--);
+		}
+
+		return list;
+	}
+
+	private static int FindBlock(this char[] list, int start)
+	{
+		var current = start;
+		var value = list[current];
+
+		while (current > 0 && list[current] == value)
+			current--;
+
+		if (current <= 0)
+			return -1;
+
+		return start - current;
+	}
+
+	private static int FindSpace(this char[] list, int start, int length)
+	{
+		var current = start;
+
+		while (length > current - start)
+		{
+			while (current < list.Length - 1 && list[current] != '.')
+				current++;
+
+			start = current;
+
+			while (current < list.Length - 1 && list[current] == '.')
+				current++;
+
+			if (current >= list.Length - 1)
+				return -1;
+		}
+
+		return start;
 	}
 }
