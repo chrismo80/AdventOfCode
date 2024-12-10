941f9801c210932619bea53808baa2efc2c2dee1
chrismo80 <krizm0@freenet.de>
2024-12-10 07:47:50 +0100

    Day 9 Part 2

--- AdventOfCode/2024/09/Day.cs
+++ AdventOfCode/2024/09/Day.cs
@@ -4,18 +4,28 @@ public static class Day9
 {
 	public static void Solve()
 	{
-		var input = File.ReadAllText("AdventOfCode/2024/09/Input.txt");
+		var diskmap = File.ReadAllText("AdventOfCode/2024/09/Input.txt")
+			.GetDiskMap();
 
-		var diskmap = input.GetDiskMap().ToArray();
+		var filesystem = diskmap.ToArray();
 
-		while (diskmap.Move())
+		while (filesystem.MoveSingle())
 		{ }
 
-		var result1 = diskmap.Where(x => x != '.').Select((c, i) => (long)(i * (c - '0'))).Sum();
-		var result2 = 0;
+		var result1 = filesystem.GetCheckSum();
+
+		filesystem = diskmap.ToArray();
+
+		while (filesystem.MoveBlock())
+		{ }
+
+		var result2 = filesystem.GetCheckSum();
 
 		Console.WriteLine($"Part 1: {result1}, Part 2: {result2}");
 	}
+
+	private static long GetCheckSum(this char[] diskmap) =>
+		diskmap.Where(x => x != '.').Select((c, i) => (long)(i * (c - '0'))).Sum();
 
 	private static string GetDiskMap(this string input) =>
 		string.Join("", input.Select((c, i) => c.Transform(i)));
@@ -23,7 +33,7 @@ public static class Day9
 	private static string Transform(this char item, int index) =>
 		new(Enumerable.Repeat(index % 2 == 0 ? (char)('0' + index / 2) : '.', item - '0').ToArray());
 
-	private static bool Move(this char[] list)
+	private static bool MoveSingle(this char[] list)
 	{
 		var digit = list.Last(x => x != '.');
 		var dot = list.First(x => x == '.');
@@ -38,5 +48,14 @@ public static class Day9
 		list[dotIndex] = digit;
 
 		return true;
+	}
+
+	private static bool MoveBlock(this char[] list)
+	{
+		var space = list.SkipWhile(x => x != '.').TakeWhile(x => x == '.');
+
+		Console.WriteLine(new string(list));
+
+		return false;
 	}
 }
