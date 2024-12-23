0afb9d2702f73c9215f88a4403ce342a435aed0e
chrismo80 <krizm0@freenet.de>
2024-12-23 12:06:36 +0100

    Rename input helper

--- AdventOfCode/InputExtensions.cs
+++ AdventOfCode/InputExtensions.cs
@@ -2,13 +2,24 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode;
 
-public static class Parser
+public static class Input
+{
+	public static string Load(int year, int day) =>
+		File.ReadAllText(FileName(year, day));
+
+	private static string FileName(int year, int day) =>
+		$"AdventOfCode/{year}/{day:00}/Input.txt";
+}
+
+public static class InputExtensions
 {
 	public static string[] Lines(this string input) =>
 		input.Split('\n');
 
 	public static char[][] ToMap(this string input) =>
-		input.Lines().Select(row => row.ToArray()).ToArray();
+		input.Lines()
+			.Select(row => row.ToArray())
+			.ToArray();
 
 	public static T[] ToArray<T>(this string input, string delimiter) =>
 		input.Split(delimiter, StringSplitOptions.RemoveEmptyEntries)
@@ -25,22 +36,14 @@ public static class Parser
 			.Select(item => item.ToNestedArray<T>(d2, d3))
 			.ToArray();
 
-	public static IEnumerable<string[]> Match(this string[] lines, Regex regex) =>
-		lines.Select(line => regex.Match(line).Groups.Values.Skip(1).Select(v => v.Value).ToArray());
-
 	public static IEnumerable<T[]> Match<T>(this string[] lines, Regex regex) =>
-		lines.Select(line => regex.Match(line).Groups.Values.Skip(1)
-			.Select(v => v.Value).ConvertTo<T>().ToArray());
+		lines.Select(line =>
+			regex.Match(line).Groups.Values.Skip(1)
+				.Select(v => v.Value).ConvertTo<T>().ToArray());
+
+	public static IEnumerable<string[]> Match(this string[] lines, Regex regex) =>
+		lines.Match<string>(regex);
 
 	public static IEnumerable<T> ConvertTo<T>(this IEnumerable<string> items) =>
 		items.Select(item => (T)Convert.ChangeType(item, typeof(T)));
-}
-
-public static class Input
-{
-	public static string Load(int year, int day) =>
-		File.ReadAllText(FileName(year, day));
-
-	private static string FileName(int year, int day) =>
-		$"AdventOfCode/{year}/{day:00}/Input.txt";
 }
