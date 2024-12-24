b929afbf9aa2a58e05b4c9d5c490e244a1607153
chrismo80 <krizm0@freenet.de>
2024-12-25 00:51:14 +0100

    Map BFS

--- AdventOfCode/MapExtensions.cs
+++ AdventOfCode/MapExtensions.cs
@@ -34,35 +34,37 @@ public static class MapExtensions
 		if (pos.Y < height - 1) yield return pos with { Y = pos.Y + 1 };
 	}
 
-	public static IEnumerable<(int X, int Y)> Bfs(this Func<(int, int), IEnumerable<(int, int)>> walkableNeighbors,
-		(int X, int Y) start, (int X, int Y) end)
+	public static IEnumerable<(int, int)> Bfs(this Func<(int, int), IEnumerable<(int, int)>> walkableNeighbors,
+		(int, int) start, (int, int) end)
 	{
-		var previous = new Dictionary<(int X, int Y), (int X, int Y)>();
-		var visited = new HashSet<(int X, int Y)> { start };
+		var previous = new Dictionary<(int, int), (int, int)>();
+		var visited = new HashSet<(int, int)> { start };
 		var active = new Queue<(int, int)>();
 
 		active.Enqueue(start);
 
-		while (active.Count > 0)
-		{
-			var current = active.Dequeue();
-
-			if (current.Equals(end))
-			{
-				while (!current.Equals(start))
-				{
-					yield return current;
-					current = previous[current];
-				}
-
-				yield break;
-			}
-
+		while (active.TryDequeue(out var current) && !current.Equals(end))
 			foreach (var neighbor in walkableNeighbors(current).Where(n => visited.Add(n)))
 			{
 				previous[neighbor] = current;
 				active.Enqueue(neighbor);
 			}
+
+		return previous.Path(end, start).Reverse();
+	}
+
+	private static IEnumerable<(int, int)> Path(this Dictionary<(int, int), (int, int)> previous,
+		(int, int) end, (int, int) start)
+	{
+		if (!previous.TryGetValue(end, out var pos))
+			yield break;
+
+		while (!pos.Equals(start))
+		{
+			yield return pos;
+			pos = previous[pos];
 		}
+
+		yield return pos;
 	}
 }
