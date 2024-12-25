90fc08a4f88fbd47f15e743baa308474e7954e5b
chrismo80 <krizm0@freenet.de>
2024-12-25 11:10:39 +0100

    Fix memory issue in 2022/18

--- AdventOfCode/2022/18/Day.cs
+++ AdventOfCode/2022/18/Day.cs
@@ -1,22 +1,27 @@
+using AdventOfCode;
+
 namespace AdventOfCode2022;
 
 public static class Day18
 {
-    public static void Solve()
-    {
-        var input = File.ReadAllLines("AdventOfCode/2022/18/Test.txt")
-            .Select(l => l.Split(',').Select(int.Parse).ToArray())
-            .Select(c => (X: c[0], Y: c[1], Z: c[2])).ToArray();
+	public static void Solve()
+	{
+		var input = Input.Load(2022, 18, "Test").Lines()
+			.Select(l => l.Split(',').Select(int.Parse).ToArray())
+			.Select(c => (X: c[0], Y: c[1], Z: c[2])).ToArray();
 
-        var result1 = input.Sum(c => 6 - BlockedSides(c, input));
-        var result2 = result1 - 0;
+		var result1 = input.Sum(c => 6 - BlockedSides(c, input));
+		var result2 = result1 - 0;
 
-        Console.WriteLine($"Part 1: {result1}, Part 2: {result2}");
+		Console.WriteLine($"Part 1: {result1}, Part 2: {result2}");
 
-        static int BlockedSides((int X, int Y, int Z) c, (int, int, int)[] all) => new List<(int, int, int)>
-            {
-                (c.X - 1, c.Y, c.Z), (c.X, c.Y - 1, c.Z), (c.X, c.Y, c.Z - 1),
-                (c.X + 1, c.Y, c.Z), (c.X, c.Y + 1, c.Z), (c.X, c.Y, c.Z + 1)
-            }.Intersect(all).Count();
-    }
+		static int BlockedSides((int X, int Y, int Z) c, (int, int, int)[] all)
+		{
+			return new List<(int, int, int)>
+			{
+				(c.X - 1, c.Y, c.Z), (c.X, c.Y - 1, c.Z), (c.X, c.Y, c.Z - 1),
+				(c.X + 1, c.Y, c.Z), (c.X, c.Y + 1, c.Z), (c.X, c.Y, c.Z + 1)
+			}.Count(all.Contains);
+		}
+	}
 }
