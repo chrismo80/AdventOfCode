735958aa4b4ac8f98254ad775c21f5a5de3d513e
chrismo80 <krizm0@freenet.de>
2024-12-25 12:20:10 +0100

    Day 10

--- AdventOfCode/2024/10/Day.cs
+++ AdventOfCode/2024/10/Day.cs
@@ -8,54 +8,34 @@ public static class Day10
 	{
 		var map = Input.Load(2024, 10).ToMap();
 
-		var trails = new List<List<(int, int)>>();
+		var starts = map.Find('0');
+		var ends = map.Find('9');
 
-		foreach (var trailHead in map.FindStarts())
-			trails.Add([trailHead]);
+		var walkable = ((int X, int Y) pos) => pos.Neighbors(map[0].Length, map.Length)
+			.Where(n => map[n.Y][n.X] - map[pos.Y][pos.X] == 1);
 
-		var first = trails.First().Run(map);
+		var trails = starts.Select(s => ends.Select(e => walkable.FindTrails(s, e))).ToList();
 
-		var result1 = 0;
-		var result2 = 0;
+		var result1 = trails.Sum(trail => trail.Count(t => t > 0));
+		var result2 = trails.Sum(trail => trail.Sum());
 
 		Console.WriteLine($"Part 1: {result1}, Part 2: {result2}");
 	}
 
-	private static List<(int X, int Y)> Run(this List<(int X, int Y)> trail, char[][] map) =>
-		// for (var i = 0; i < count; i++)
-		// {
-		// 	var current = trails[i].Last();
-		// 	var height = map[current.Y][current.X];
-		//
-		// 	if (height == '9')
-		// 		continue;
-		//
-		// 	foreach (var neighbour in current.Neighbors(map).Where(p => map[p.Y][p.X] != height + 1))
-		// 	{
-		// 		var newTrail = trails[i].Append(neighbour).ToList();
-		// 		trails.Add(newTrail);
-		// 	}
-		//
-		// 	trails.Remove(trails[i]);
-		// }
-		//
-		// return trails;
-		default;
-
-	private static IEnumerable<(int X, int Y)> FindStarts(this char[][] map)
+	private static int FindTrails<T>(this Func<T, IEnumerable<T>> walkableNeighbors, T start, T end)
 	{
-		foreach (var (x, y) in from y in Enumerable.Range(0, map.Length)
-				from x in Enumerable.Range(0, map[0].Length)
-				select (x, y))
-			if (map[y][x] == '0')
-				yield return (x, y);
-	}
+		var visited = 0;
+		var active = new Queue<T>([start]);
 
-	private static IEnumerable<(int X, int Y)> Neighbors(this (int X, int Y) position, char[][] map)
-	{
-		if (position.Y > 0) yield return (position.X, position.Y - 1);
-		if (position.X > 0) yield return (position.X - 1, position.Y);
-		if (position.Y < map.Length - 1) yield return (position.X, position.Y + 1);
-		if (position.X < map[0].Length - 1) yield return (position.X + 1, position.Y);
+		while (active.TryDequeue(out var current))
+			foreach (var neighbor in walkableNeighbors(current))
+			{
+				if (neighbor!.Equals(end))
+					visited++;
+
+				active.Enqueue(neighbor);
+			}
+
+		return visited;
 	}
 }
