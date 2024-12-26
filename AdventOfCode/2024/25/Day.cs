13254fc1d2f7395e29148f9f416660cdf4b5804e
chrismo80 <krizm0@freenet.de>
2024-12-26 14:25:00 +0100

    Day 25

--- AdventOfCode/2024/25/Day.cs
+++ AdventOfCode/2024/25/Day.cs
@@ -1,5 +1,4 @@
 using AdventOfCode;
-using NSubstitute.Extensions;
 
 namespace AdventOfCode2024;
 
@@ -9,17 +8,17 @@ public static class Day25
 	{
 		var input = Input.Load(2024, 25).ToArray<string>("\n\n");
 
-		var locks = input.Where(item => item.First() == '#').Select(x => x.Parse()).ToArray();
-		var keys = input.Where(item => item.Last() == '#').Select(x => x.Parse()).ToArray();
+		var locks = input.Where(item => item.First() == '#').Select(Parse).ToArray();
+		var keys = input.Where(item => item.Last() == '#').Select(Parse).ToArray();
 
-		var fits = locks.Sum(l => keys.Count(k => l.Fit(k)));
+		var fits = keys.Sum(k => locks.Count(l => k.Fit(l)));
 
 		Console.WriteLine($"Result: {fits}");
 	}
 
-	private static int[] Parse(this string input) =>
-		input.Lines().Skip(1).SkipLast(1).Transpose().Select(row => row.Replace(".", "").Length).ToArray();
+	private static int[] Parse(this string input) => input.Lines().Skip(1).SkipLast(1)
+		.Transpose().Select(row => row.Replace(".", "").Length).ToArray();
 
-	private static bool Fit(this int[] lockHeights, int[] keyHeights, int space = 5) =>
-		lockHeights.Zip(keyHeights, (l, k) => l + k).All(fit => fit <= space);
+	private static bool Fit(this int[] keyHeights, int[] lockHeights, int space = 5) =>
+		keyHeights.Zip(lockHeights, (k, l) => k + l).All(fit => fit <= space);
 }
