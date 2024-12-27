321dc02675537f2b96f955775e47be2a6c279a56
chrismo80 <krizm0@freenet.de>
2024-12-27 21:52:01 +0100

    Day 19

--- AdventOfCode/2024/19/Day.cs
+++ AdventOfCode/2024/19/Day.cs
@@ -13,25 +13,25 @@ public static class Day19
 		var towels = input.First().ToArray<string>(", ");
 		var designs = input.Last().ToArray<string>("\n");
 
-		var checks = designs.AsParallel().Select(design =>
-			design.CountWays(towels.Where(t => design.Contains(t)).ToArray()));
+		var checks = designs.AsParallel()
+			.Select(design => design.CountWays(towels.Where(t => design.Contains(t)).ToArray()));
 
 		Console.WriteLine($"Part 1: {checks.Count(ways => ways > 0)}, Part 2: {checks.Sum()}");
 	}
 
 	private static long CountWays(this string remaining, string[] towels)
 	{
-		// remaining is empty, so fully doable, add one more way
+		// remaining is empty, so design fully doable, add one more way
 		if (remaining.Length == 0)
 			return 1;
 
-		// if already calculated just return value
+		// if already calculated, just return number of ways for pattern
 		if (_cache.TryGetValue(remaining, out var count))
 			return count;
 
-		// not yet calculated, do so and store in cache
-		_cache[remaining] = towels.Where(remaining.StartsWith)
-			.Sum(towel => remaining[towel.Length..].CountWays(towels));
+		// not yet calculated, do so and store number of ways in cache
+		_cache[remaining] =
+			towels.Where(remaining.StartsWith).Sum(towel => remaining[towel.Length..].CountWays(towels));
 
 		// then return value from cache
 		return _cache[remaining];
