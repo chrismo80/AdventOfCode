02a1ce284390581ef6880931d92f1c4881eb267a
chrismo80 <krizm0@freenet.de>
2024-12-29 01:11:19 +0100

    Day 24 Part 1

--- AdventOfCode/2024/24/Day.cs
+++ AdventOfCode/2024/24/Day.cs
@@ -8,30 +8,38 @@ public static class Day24
 	{
 		var input = Input.Load(2024, 24).ToArray<string>("\n\n");
 
-		var values = input.First().ToNestedArray<string>("\n", ": ")
+		var initialValues = input.First().ToNestedArray<string>("\n", ": ")
 			.ToDictionary(kvp => kvp[0], kvp => int.Parse(kvp[1]));
 
 		var connections = input.Last().ToNestedArray<string>("\n", " ");
 
-		while (connections.FirstOrDefault(c => !values.ContainsKey(c.Last())) != null)
-			foreach (var connection in connections)
-			{
-				if (values.ContainsKey(connection.Last()))
-					continue;
+		var expected = initialValues.Value('x') + initialValues.Value('y');
 
-				if (!values.ContainsKey(connection[0]) || !values.ContainsKey(connection[2]))
-					continue;
-
-				values[connection.Last()] = Connect(values[connection[0]], connection[1], values[connection[2]]);
-			}
-
-		var zValues = values.Where(kvp => kvp.Key.StartsWith("z")).OrderByDescending(kvp => kvp.Key)
-			.Select(kvp => kvp.Value);
-
-		var result1 = Convert.ToInt64(string.Join("", zValues), 2);
-		var result2 = 0;
+		var result1 = initialValues.Calculate(connections).Value('z');
+		var result2 = expected;
 
 		Console.WriteLine($"Part 1: {result1}, Part 2: {result2}");
+	}
+
+	private static Dictionary<string, int> Calculate(this Dictionary<string, int> initialValues, string[][] connections)
+	{
+		var dict = initialValues.ToDictionary(entry => entry.Key, entry => entry.Value);
+
+		while (connections.Any(c => !dict.ContainsKey(c.Last())))
+			foreach (var connection in connections.Where(c =>
+						!dict.ContainsKey(c.Last()) && dict.ContainsKey(c[0]) && dict.ContainsKey(c[2])))
+				dict[connection.Last()] = Connect(dict[connection[0]], connection[1], dict[connection[2]]);
+
+		return dict;
+	}
+
+	private static long Value(this Dictionary<string, int> dict, char start)
+	{
+		var filtered = dict.Where(entry => entry.Key.StartsWith(start))
+			.OrderByDescending(entry => entry.Key)
+			.Select(entry => entry.Value);
+
+		return Convert.ToInt64(string.Join("", filtered), 2);
 	}
 
 	private static int Connect(int left, string gate, int right) => gate switch
