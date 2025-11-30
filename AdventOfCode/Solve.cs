c04680a1fb84268d82baf0a6dfe16496ba0b3623
chrismo80 <krizm0@freenet.de>
2025-11-30 15:24:32 +0100

    code format

--- AdventOfCode/Solve.cs
+++ AdventOfCode/Solve.cs
@@ -6,15 +6,12 @@ using System.Diagnostics;
 
 public static class Problem
 {
-	public static void Solve(int year, int day)
+	public static void Solve((int Year, int Day) problem)
 	{
-		if (!Input.Exists(year, day))
-			return;
-
-		var input = Input.Load(year, day);
+		var input = Input.Load(problem.Year, problem.Day);
 
 		var method = Assembly.GetExecutingAssembly()
-			.GetType($"AdventOfCode{year}.Day{day}")
+			.GetType($"AdventOfCode{problem.Year}.Day{problem.Day}")
 			.GetMethod("Solve");
 
 		long start = Stopwatch.GetTimestamp(), i = 1;
@@ -22,7 +19,7 @@ public static class Problem
 		if (method.Invoke(null, [input]) is not IEnumerable<object> results)
 			return;
 
-		Console.Write($"Year {year} Day {day:00}: {Stopwatch.GetElapsedTime(start).TotalMilliseconds:F1} ms".PadRight(40));
+		Console.Write($"Year {problem.Year} Day {problem.Day:00}: {Stopwatch.GetElapsedTime(start).TotalMilliseconds:F1} ms".PadRight(40));
 
 		foreach (var result in results)
 			Console.Write($"Result {i++}: {result}".PadRight(30));
--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
-﻿var years = Enumerable.Range(2015, 11);
+﻿var years = Enumerable.Range(2015, 15);
 var days = Enumerable.Range(1, 25);
 
 var problems = years.SelectMany(year => days.Select(day => (year, day)))
 	.Where(problem => AdventOfCode.Input.Exists(problem.year, problem.day))
 	.ToArray();
 
-var (year, day) = problems.Last();
-
-AdventOfCode.Problem.Solve(year, day);
+AdventOfCode.Problem.Solve(problems.Last());
