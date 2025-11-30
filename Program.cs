553820cf0e9cdae8ab4c4eb4a11ad7d654b78dad
chrismo80 <krizm0@freenet.de>
2025-11-30 15:14:03 +0100

    execute always last problem only

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿for (var year = 2025; year <= 2025; year++)
-for (var day = 1; day <= 25; day++)
-	AdventOfCode.Problem.Solve(year, day);
+﻿var years = Enumerable.Range(2015, 11);
+var days = Enumerable.Range(1, 25);
+
+var problems = years.SelectMany(year => days.Select(day => (year, day)))
+	.Where(problem => AdventOfCode.Input.Exists(problem.year, problem.day))
+	.ToArray();
+
+var (year, day) = problems.Last();
+
+AdventOfCode.Problem.Solve(year, day);
