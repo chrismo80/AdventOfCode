b4ee5c9d6b0ba8c47e9eeeaed7361b010b047c30
chrismo80 <krizm0@freenet.de>
2025-12-02 21:12:59 +0100

    day 1

--- AdventOfCode/2025/01/Day.cs
+++ AdventOfCode/2025/01/Day.cs
@@ -9,11 +9,12 @@ public static class Day1
 		var steps = input.Lines().Select(s => int.Parse(s.Replace("L", "-").Replace("R", "+")));
 
 		yield return CountZeros(steps);
+		yield return CountClicks(steps);
 	}
 
-	private static int CountZeros(IEnumerable<int> steps)
+	private static int CountZeros(IEnumerable<int> steps, int position = 50)
 	{
-		int position = 50, count = 0;
+		var count = 0;
 
 		foreach (var step in steps)
 		{
@@ -22,6 +23,38 @@ public static class Day1
 
 			if (position == 0)
 				count++;
+		}
+
+		return count;
+	}
+
+	private static int CountClicks(IEnumerable<int> steps, int position = 50)
+	{
+		var count = 0;
+
+		foreach (var step in steps)
+		{
+			var distance = step;
+
+			while (distance-- > 0)
+			{
+				position++;
+				position %= 100;
+
+				if (position == 0)
+					count++;
+			}
+
+			distance = step;
+
+			while (distance++ < 0)
+			{
+				position--;
+				position %= 100;
+
+				if (position == 0)
+					count++;
+			}
 		}
 
 		return count;
