3e1ab5972f966f366343697a76ca5b9904dda2a7
chrismo80 <krizm0@freenet.de>
2025-12-04 18:01:00 +0100

    part 1

--- AdventOfCode/2025/03/Day.cs
+++ AdventOfCode/2025/03/Day.cs
@@ -6,6 +6,19 @@ public static class Day3
 {
 	public static IEnumerable<object> Solve(string input)
 	{
-		yield return 0;
+		var banks = input.Lines().Select(s => s.Select(c => c - '0').ToList());
+
+		yield return banks.Sum(LargestJoltage);
+	}
+
+	private static int LargestJoltage(List<int> bank)
+	{
+		var max = bank.Max();
+		var pos = bank.IndexOf(max);
+
+		var left = bank.Take(pos).Select(x => int.Parse($"{x}{max}"));
+		var right = bank.Skip(pos + 1).Select(x => int.Parse($"{max}{x}"));
+
+		return Math.Max(left.Append(0).Max(), right.Append(0).Max());
 	}
 }
