abd20d7e8fcfc31af7d74cd519fd752340a47a73
chrismo80 <krizm0@freenet.de>
2025-12-06 19:48:09 +0100

    5.2

--- AdventOfCode/2025/05/Day.cs
+++ AdventOfCode/2025/05/Day.cs
@@ -13,5 +13,21 @@ public static class Day5
 		var ids = parts[1].ToArray<long>("\n");
 
 		yield return ids.Count(id => ranges.Any(range => id >= range.First() && id <= range.Last()));
+
+		ranges = ranges.OrderBy(r => r[0]).ToArray();
+
+		var mergedRanges = new List<long[]> { ranges.First() };
+
+		foreach (var range in ranges.Skip(1))
+		{
+			var last = mergedRanges.Last();
+
+			if (range[0] > last[1])
+				mergedRanges.Add(range);
+			else
+				last[1] = Math.Max(last[1], range[1]);
+		}
+
+		yield return mergedRanges.Sum(r => r[1] - r[0] + 1);
 	}
 }
